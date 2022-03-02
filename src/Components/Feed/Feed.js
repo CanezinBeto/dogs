acbdc562bc50709809dd6e850e56b714b6fe9dd1
CanezinBeto <albertocanezin@gmail.com>
2022-03-02 13:14:17 -0300

    Iniciando Feed

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import FeedModal from './FeedModal'
+import FeedPhotos from './FeedPhotos'
 
 const Feed = () => {
-  return <div>Feed</div>
+  const [modalPhoto, setModalPhotos] = React.useState(null)
+
+  return (
+    <div>
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} setModalPhotos={setModalPhotos} />
+      )}
+      <FeedPhotos setModalPhotos={setModalPhotos} />
+    </div>
+  )
 }
 
 export default Feed
