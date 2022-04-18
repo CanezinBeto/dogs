3ebd5ba76af624c6f137a08d8330fae487ede1ed
CanezinBeto <albertocanezin@gmail.com>
2022-04-18 18:53:53 -0300

    Ajuste na rota de visualização de perfis

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -20,7 +20,7 @@ const PhotoContent = ({ data, single }) => {
             {user.data && user.data.username === photo.author ? (
               <PhotoDelete id={photo.id} />
             ) : (
-              <Link to={`/perfil${photo.author}`}>@{photo.author}</Link>
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             )}
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
